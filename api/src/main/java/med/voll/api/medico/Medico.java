5f0ddc5f54fa292d7f4734a941f7b16f5293917a
Paulojoserc <pjrc83@gmail.com>
2023-06-08 23:16:07 -0300

    Cap Spring Data JPA
    Adicionar novas dependências no projeto;
    Mapear uma entidade JPA e criar uma interface Repository para ela;
    Utilizar o Flyway como ferramenta de Migrations do projeto;
    Realizar validações com Bean Validation utilizando algumas de suas anotações, como a @NotBlank.

--- api/src/main/java/med/voll/api/medico/Medico.java
+++ api/src/main/java/med/voll/api/medico/Medico.java
@@ -14,15 +14,25 @@ import med.voll.api.endereco.Endereco;
 @AllArgsConstructor
 @EqualsAndHashCode(of = "id")
 public class Medico {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
     private String email;
+    private String telefone;
     private String crm;
     @Enumerated(EnumType.STRING)
     private Especialidade especialidade;
     @Embedded
     private Endereco endereco;
 
+    public Medico(DadosCadastroMedico dados) {
+        this.nome = dados.nome();
+        this.email = dados.email();
+        this.telefone = dados.Telefone();
+        this.crm = dados.crm();
+        this.especialidade = dados.especialidade();
+        this.endereco =  new Endereco(dados.endereco());
+    }
 }
